a3373d425e3454f8cdc2d04875cf802bcfb8a3f9
Weslley Evangelista <weslleylello@gmail.com>
2023-12-09 10:44:59 -0300

    desafio finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia smartphone1 = new(numero: "123456789", modelo: "nokia1", imei: "abc1234", memoria: 1024);
+Iphone smartphone2 = new(numero: "987654321", modelo: "iphone1", imei: "def5678", memoria: 1024);
+
+smartphone1.Ligar();
+smartphone1.ReceberLigacao();
+smartphone1.InstalarAplicativo(nomeApp: "Whatsapp");
+
+smartphone1.Ligar();
+smartphone2.ReceberLigacao();
+smartphone2.InstalarAplicativo(nomeApp: "Telegram");
